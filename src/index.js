ebf597f1cc7f848cc60931b53b9e75595305ec55
Lenny Brown <penn2014@gmail.com>
2020-04-19 16:46:39 -0700

    Added dummy data and redux with reducers

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { createStore } from 'redux';
+import rootReducer from './store/reducers/rootReducer';
 
-const store = createStore();
+const store = createStore(rootReducer);
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
