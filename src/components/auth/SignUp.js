ef948ef04cc47d130921899619e0e2c505541766
Lenny Brown <penn2014@gmail.com>
2020-04-20 16:34:25 -0700

    Added signing up and error checking for signing up

--- src/components/auth/SignUp.js
+++ src/components/auth/SignUp.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 
+import { signUp } from '../../store/actions/authActions';
+
 export class SignUp extends Component {
     state = {
         email: '',
@@ -18,11 +20,11 @@ export class SignUp extends Component {
 
     handleSubmit = (e) => {
         e.preventDefault();
-        console.log(this.state);
+        this.props.signUp(this.state);
     }
 
     render() {
-      const { auth } = this.props;
+      const { auth, authError } = this.props;
       if (auth.uid) return <Redirect to='/' />
 
         return (
@@ -47,6 +49,9 @@ export class SignUp extends Component {
                 </div>
                 <div className="input-field">
                   <button className="btn pink lighten-1 z-depth-0">Login</button>
+                  <div className="red-text center">
+                    { authError ? <p>{ authError }</p> : null}
+                  </div>
                 </div>
               </form>
             </div>
@@ -56,8 +61,15 @@ export class SignUp extends Component {
 
 const mapStateToProps = (state) => {
   return {
-    auth: state.firebase.auth
+    auth: state.firebase.auth,
+    authError: state.auth.authError
   }
 }
 
-export default connect(mapStateToProps)(SignUp);
+const mapDispatchToProps = (dispatch) => {
+  return {
+    signUp: (newUser) => dispatch(signUp(newUser))
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SignUp);
